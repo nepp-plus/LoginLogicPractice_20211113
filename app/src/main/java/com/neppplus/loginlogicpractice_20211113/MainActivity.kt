acfd3016aa5166d7059438404b1d8d5c205307a0
nepp-plus <nepp.plus@gmail.com>
2021-11-13 14:25:47 +0900

    코틀린 이벤트처리  - UI 속성 - get  : 변수에 저장  - if 조건문 -> 비교 / 논리 활용  - 토스트로 문구 띄우기

--- app/src/main/java/com/neppplus/loginlogicpractice_20211113/MainActivity.kt
+++ app/src/main/java/com/neppplus/loginlogicpractice_20211113/MainActivity.kt
@@ -2,10 +2,36 @@ package com.neppplus.loginlogicpractice_20211113
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnLogIn.setOnClickListener {
+
+//            ID / PW에 입력된 내용 (text) -> 변수에 저장해두자.
+
+            val inputId =  edtId.text.toString()
+            val inputPw =  edtPassword.text.toString()
+
+//            조건문으로 ID / PW이 맞는지? 질문
+//            1. inputId 값이, "admin"인가?
+//            2. 1과 동시에 만족,  inputPw의 값이, "qwer" 인가?
+
+            if ( inputId == "admin" && inputPw == "qwer") {
+
+                Toast.makeText(this, "관리자님 환영합니다.", Toast.LENGTH_SHORT).show()
+
+            }
+            else {
+                Toast.makeText(this, "로그인에 실패했습니다.", Toast.LENGTH_SHORT).show()
+            }
+
+
+        }
+
     }
 }
